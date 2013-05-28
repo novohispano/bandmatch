5f1d5736bea93e9a30f0f6e261703c5d46dac432
novohispano@gmail.com <novohispano@gmail.com>
2013-05-28 16:40:55 -0600

    Rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bandmatch::Application.config.secret_token = 'f961c1b663475b1301f964fe6055e3eaabdf4297f138c6cbec3c32c4ba5f5ca085ee4c60520e6a304ac1d4f9934dde8e70ed5e2f068750594c2b0b410f628764'
