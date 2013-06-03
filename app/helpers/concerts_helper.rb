7b419436aa8a00f1a79a3d3cdcb7a02b4ee5aba9
novohispano@gmail.com <novohispano@gmail.com>
2013-06-02 19:07:21 -0600

    Added some formatting on the artists

--- app/helpers/concerts_helper.rb
+++ app/helpers/concerts_helper.rb
@@ -1,2 +1,11 @@
 module ConcertsHelper
+  def artists_from(performances)
+    artists = ""
+
+    performances.each do |performance|
+      artists << "#{performance.artist.display_name}, "
+    end
+
+    artists.chop.chop
+  end
 end
